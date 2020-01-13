dc40cb637a38c696b53711124df570f2ff45bb6a
Ryan Bahan <ryan@ryanbahan.com>
2020-01-12 20:39:29 -0700

    Add functionality to mark cards urgent + persist state on page refresh

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var taskListTitleInput = document.querySelector('.task-title-field');
 var cardColumnOne = document.querySelector('.card-column-one');
 var cardColumnTwo = document.querySelector('.card-column-two');
 var submitBtn = document.querySelector('.make-task-list');
+var mainCardsDisplay = document.querySelector('.cards-display-container');
 var toDoCards = [];
 
 addTaskItemBtn.addEventListener('click', addTaskItem);
@@ -14,6 +15,7 @@ formTaskItems.addEventListener('click', removeTaskItems);
 clearAllBtn.addEventListener('click', clearForm);
 taskListForm.addEventListener('submit', submitForm);
 taskListForm.addEventListener('input', checkFormValidity);
+mainCardsDisplay.addEventListener('click', toggleUrgent)
 
 gettoDoCards();
 loadToDoCards();
@@ -71,16 +73,16 @@ function getTaskItems(toDoList) {
 function displayToDoCard(toDoList) {
     if (cardColumnOne.children.length > cardColumnTwo.children.length) {
         cardColumnTwo.insertAdjacentHTML('afterbegin', 
-            `<section class="task-list">
+            `<section class="task-list" data-id="${toDoList.id}">
             <div class="task-title">${toDoList.title}</div>
             <div class="task-items-list" id="${toDoList.id}">
             </div>
             <div class="task-footer">
-                <div class="card-footer-img-wrapper">
-                    <img src="./assets/urgent.svg" alt="" class="urgent-img">
+                <div class="card-footer-img-wrapper urgent-btn">
+                    <img src="./assets/urgent.svg" alt="" class="urgent-img" data-id="${toDoList.id}">
                     <p>URGENT</p>
                 </div>
-                <div class="card-footer-img-wrapper">
+                <div class="card-footer-img-wrapper delete-btn">
                     <img src="./assets/delete.svg" alt="" class="delete-img">
                     <p>DELETE</p>
                 </div>
@@ -94,18 +96,19 @@ function displayToDoCard(toDoList) {
                 <p>${task.title}</p>
                 </div>
             `)});
+        displayUrgentCard(toDoList);
     } else {
         cardColumnOne.insertAdjacentHTML('afterbegin', 
-            `<section class="task-list">
+            `<section class="task-list" data-id="${toDoList.id}">
             <div class="task-title">${toDoList.title}</div>
             <div class="task-items-list" id="${toDoList.id}">
             </div>
             <div class="task-footer">
-                <div class="card-footer-img-wrapper">
-                    <img src="./assets/urgent.svg" alt="" class="urgent-img">
+                <div class="card-footer-img-wrapper urgent-btn">
+                    <img src="./assets/urgent.svg" alt="" class="urgent-img" data-id="${toDoList.id}">
                     <p>URGENT</p>
                 </div>
-                <div class="card-footer-img-wrapper">
+                <div class="card-footer-img-wrapper delete-btn">
                     <img src="./assets/delete.svg" alt="" class="delete-img">
                     <p>DELETE</p>
                 </div>
@@ -119,6 +122,16 @@ function displayToDoCard(toDoList) {
                 <p>${task.title}</p>
                 </div>
             `)});
+        displayUrgentCard(toDoList);
+    }
+}
+
+function displayUrgentCard(toDoList) {
+    if (toDoList.urgent === true) {
+        var id = toDoList.id.toString();
+        var card = document.querySelector(`section[data-id="${id}"]`)
+        card.classList.add('urgent');
+        toggleActiveImg(toDoList, 'urgent');
     }
 }
 
@@ -127,6 +140,7 @@ function gettoDoCards() {
         var toDoCardKey = localStorage.key([i]);
         if (toDoCardKey.includes('toDoList')) {
             var toDoCard = JSON.parse(window.localStorage.getItem(toDoCardKey));
+            toDoCard = new ToDoList(toDoCard.id, toDoCard.title, toDoCard.tasks, toDoCard.urgent);
             toDoCards.push(toDoCard);
         }
     }
@@ -149,3 +163,24 @@ function checkFormValidity() {
         clearAllBtn.setAttribute('disabled', "");
     }
 };
+
+function toggleUrgent() {
+    if (event.target.parentNode.classList.contains('urgent-btn')) {
+        var displayCard = event.target.parentNode.parentNode.parentNode;
+        var savedToDo = toDoCards.find(card => card.id == displayCard.dataset.id);
+        toggleActiveImg(savedToDo, 'urgent');
+        // var urgentImg = document.querySelector(`img[data-id="${savedToDo.id}"]`);
+        // urgentImg.src = "./assets/urgent-active.svg";
+        savedToDo.updateToDo();
+        displayCard.classList.toggle('urgent');
+    }
+}
+
+function toggleActiveImg(card, type) {
+    var displayImg = document.querySelector(`img[data-id="${card.id}"]`);
+    if (displayImg.src.includes('active')) {
+        displayImg.src = `./assets/${type}.svg`;
+    } else {
+        displayImg.src = `./assets/${type}-active.svg`;
+    }
+}
--- to-do-list.js
+++ to-do-list.js
@@ -1,9 +1,9 @@
 class ToDoList {
-    constructor(id, title) {
+    constructor(id, title, tasks, urgent) {
         this.id = id;
         this.title = title;
-        this.urgent = false;
-        this.tasks = [];
+        this.urgent = urgent || false;
+        this.tasks = tasks || [];
     }
     saveToStorage() {
         window.localStorage.setItem(`toDoList ${this.title} ${this.id}`, JSON.stringify(this));
@@ -12,7 +12,12 @@ class ToDoList {
 
     }
     updateToDo() {
-
+        if (this.urgent === false) {
+            this.urgent = true;
+        } else {
+            this.urgent = false;
+        }
+        this.saveToStorage();
     }
     updateTask() {
         
